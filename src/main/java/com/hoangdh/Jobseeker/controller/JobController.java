d0acd4765bc95e3aa867ed6562aa011703326f59
Hoang <dohuyhoang1120@gmail.com>
2022-06-24 09:56:19 +0700

    change api path

--- src/main/java/com/hoangdh/Jobseeker/controller/JobController.java
+++ src/main/java/com/hoangdh/Jobseeker/controller/JobController.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,8 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hoangdh.Jobseeker.model.Job;
 import com.hoangdh.Jobseeker.repository.JobRepository;
 
+@CrossOrigin
 @RestController
-@RequestMapping("/api/jobs")
+@RequestMapping("/api/job")
 public class JobController {
 
 	@Autowired 
@@ -33,24 +37,36 @@ public class JobController {
 		return jobRepository.findAll();
 	}
 	
-	@GetMapping("/job/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<Job> getJobById(@PathVariable(value = "id") int jobId) throws Exception{
 		Job job = jobRepository.findById(jobId)
 		          .orElseThrow(() -> new Exception("Job not found for this id :: " + jobId));
 		        return ResponseEntity.ok().body(job);
 	}
 	
-	@PostMapping("/job/add")
-	public ResponseEntity<Job> addJob(@RequestBody Job newJob){
-		Job jobCheck = jobRepository.getOne(newJob.getId());
+	@GetMapping("/search/{search}")
+	public List<Job> findByTitle(@PathVariable(value = "search") String search) throws Exception{
+		List<Job> result = (List<Job>) jobRepository.findByJobTitleContains(search);
+		return result;
+	}
+	
+//	@GetMapping("/searchByCategory/{categoryId}")
+//	public List<Job> findByCategory(@PathVariable(value = "categoryId") String id) throws Exception{
+//		List<Job> result = (List<Job>) jobRepository.findByCategoryId(id);
+//		return result;
+//	}
+	
+	@PostMapping(value = "/add")
+	public ResponseEntity<Job> addJob(@RequestBody Job job){
+		Job jobCheck = jobRepository.getOne(job.getId());
 		if(jobCheck == null) {
 			return ResponseEntity.notFound().build();
 		}
-		jobRepository.save(newJob);
-		return ResponseEntity.ok().body(newJob);
+		jobRepository.save(job);
+		return ResponseEntity.ok().body(job);
 	}
 	
-	@RequestMapping(value = "/job/update/{id}", method = RequestMethod.PUT)
+	@PutMapping(value = "/update/{id}")
 	public ResponseEntity<Job> updateJob(@PathVariable(value = "id") int id, @RequestBody Job job){
 		Job result = jobRepository.getOne(id);
 		if(result == null) {
@@ -61,7 +77,7 @@ public class JobController {
 		return ResponseEntity.ok().body(result);
 	}
 	
-	@RequestMapping(value = "/job/delete/{id}",method = RequestMethod.DELETE)
+	@DeleteMapping(value = "/delete/{id}")
 	public ResponseEntity<Job> deleteJob(@PathVariable(value = "id") int id){
 		Job result = jobRepository.getOne(id);
 		if(result == null) {
--- src/main/java/com/hoangdh/Jobseeker/repository/JobRepository.java
+++ src/main/java/com/hoangdh/Jobseeker/repository/JobRepository.java
@@ -1,5 +1,7 @@
 package com.hoangdh.Jobseeker.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.hoangdh.Jobseeker.model.Job;
 
 @Repository
 public interface JobRepository extends JpaRepository<Job, Integer>{
+
+	List<Job> findByJobTitleContains(String search);
 	
 }
