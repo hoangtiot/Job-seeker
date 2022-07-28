bde55f12bcdbe9b396156957e17152c9d70850fa
huymannhhuy17800 <huymanhhuy178@gmail.com>
2022-07-28 14:01:09 +0700

    add company in semester

--- src/main/java/com/hoangdh/Jobseeker/model/CompanyInSemester.java
+++ src/main/java/com/hoangdh/Jobseeker/model/CompanyInSemester.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -16,12 +18,36 @@ public class CompanyInSemester implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
+	@ManyToOne
+	@JoinColumn(name = "SemesterID")
+	private Semester semester;
+	
+	@ManyToOne
+	@JoinColumn(name = "CompanyID")
+	private Company company;
+	
 	public int getId() {
 		return id;
 	}
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public Semester getSemester() {
+		return semester;
+	}
+
+	public void setSemester(Semester semester) {
+		this.semester = semester;
+	}
+
+	public Company getCompany() {
+		return company;
+	}
+
+	public void setCompany(Company company) {
+		this.company = company;
 	}
 		
 }
---
+++ src/main/java/com/hoangdh/Jobseeker/repository/CompanyInSemesterRepository.java
@@ -0,0 +1,9 @@
+package com.hoangdh.Jobseeker.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hoangdh.Jobseeker.model.CompanyInSemester;
+
+public interface CompanyInSemesterRepository extends JpaRepository<CompanyInSemester, Integer> {
+
+}
