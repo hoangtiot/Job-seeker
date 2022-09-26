a2daf986e260c48e0edd1a83634f7f56ce074dc4
phuoctung <nguyenphuoctung.284@gmail.com>
2022-09-27 00:39:44 +0700

    first commit

--- src/main/java/com/hoangdh/Jobseeker/repository/CompanyInSemesterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hoangdh.Jobseeker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hoangdh.Jobseeker.model.CompanyInSemester;
-
-public interface CompanyInSemesterRepository extends JpaRepository<CompanyInSemester, Integer> {
-
-}
--- src/main/java/com/hoangdh/Jobseeker/repository/MajorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hoangdh.Jobseeker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hoangdh.Jobseeker.model.Major;
-
-public interface MajorRepository extends JpaRepository<Major, Integer>{
-
-}
